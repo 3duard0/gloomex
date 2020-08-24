40155a756b90b22ec8a46f1b6a7490d288f62f1e
3duard0 <eduardo.seabra@stone.com.br>
2020-08-24 09:36:33 -0300

    chore: prepare for circleci and hex

--- mix.exs
+++ mix.exs
@@ -4,28 +4,79 @@ defmodule Gloomex.MixProject do
   def project do
     [
       app: :gloomex,
+      description: description(),
+      package: package(),
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixirc_paths: elixirc_paths(Mix.env()),
+      test_coverage: [tool: ExCoveralls],
+      deps: deps(),
+      aliases: aliases(),
+      dialyzer: [lt_add_apps: [:ex_unit]],
+      preferred_cli_env: [
+        quality: :test,
+        "quality.ci": :test
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  def elixirc_paths(:test), do: ["lib", "test/support"]
+  def elixirc_paths(_), do: ["lib"]
+
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    if Mix.env() == :test do
+      [
+        extra_applications: [:logger, :runtime_tools]
+      ]
+    else
+      [
+        extra_applications: [:logger]
+      ]
+    end
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:murmur, "~> 1.0.1"},
+
+      # Test
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:murmur, "~> 1.0.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.11", only: :test},
+      {:junit_formatter, "~> 3.0", only: :test, override: true},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp aliases do
+    [
+      quality: [
+        "format",
+        "credo --strict",
+        "coveralls.html -u",
+        "dialyzer"
+      ],
+      "quality.ci": [
+        "format --check-formatted",
+        "compile --force --warnings-as-errors",
+        "credo --strict",
+        "coveralls.html -u --raise",
+        "dialyzer --halt-exit-status"
+      ]
+    ]
+  end
+
+  defp description do
+    "Guava like bloom filter library"
+  end
+
+  defp package do
+    [
+      organization: "stone",
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Copyright"],
+      links: %{"GitHub" => "https://github.com/dlpco/gloomex"}
     ]
   end
 end
